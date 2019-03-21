38095f08d0e5626e521902408913c9eb34fe4e67
drnkwati <drnkwati@yahoo.com>
2019-03-22 03:05:41 +0400

    first commit

---
+++ src/Dumper/Dumper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Repack\Dumper;
+
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+
+class Dumper
+{
+    /**
+     * Dump a value with elegance.
+     *
+     * @param  mixed  $value
+     * @return void
+     */
+    public function dump($value)
+    {
+        $dumper = in_array(PHP_SAPI, ['cli', 'phpdbg']) ? new CliDumper : new HtmlDumper;
+
+        $cloner = new VarCloner;
+
+        $dumper->dump($cloner->cloneVar($value));
+    }
+}
---
+++ src/helpers.php
@@ -0,0 +1,32 @@
+<?php
+
+use Repack\Dumper;
+
+if (!function_exists('dd')) {
+    /**
+     * Dump the passed variables and end the script.
+     *
+     * @param  dynamic mixed
+     * @return void
+     */
+    function dd()
+    {
+        array_map(function ($x) {
+            $dumper = new Dumper;
+            $dumper->dump($x);
+        }, func_get_args());
+
+        exit(1);
+    }
+}
+
+if (!function_exists('de')) {
+    function de()
+    {
+        array_map(function ($x) {
+            echo $x;
+        }, func_get_args());
+
+        exit(1);
+    }
+}
